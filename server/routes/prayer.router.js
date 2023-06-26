987b2ec4997428fab8dda07f1569e599e58c59bc
Mahamud Aden <mahamudadn@gmail.com>
2023-06-26 11:50:11 -0500

    added a get from the router side

--- server/routes/prayer.router.js
+++ server/routes/prayer.router.js
@@ -7,6 +7,15 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
+ const queryText = `SELECT * FROM  "weekly_history"`;
+  pool.query(queryText)
+  .then(result => {
+    res.send(result.rows)
+  })
+  .catch(err => {
+    console.log('error getting prayers', err);
+    res.sendStatus(200);
+  })
 });
 
 /**
