472a3940949e9615ca547bee9d31b589423373e7
Mahamud Aden <mahamudadn@gmail.com>
2023-06-26 15:36:43 -0500

    added a hisory reducer

---
+++ src/redux/reducers/history.reducer.js
@@ -0,0 +1,11 @@
+const hisoryReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_ITEMS':
+            return action.payload;
+            default:
+                return state;
+    }
+        
+}
+
+export default hisoryReducer;
