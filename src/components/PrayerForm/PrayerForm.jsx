f05235cfb1ebeb84071e7028a57d5f0a01e80212
Mahamud Aden <mahamudadn@gmail.com>
2023-06-27 10:02:38 -0500

    added dispatch and usestaes required

---
+++ src/components/PrayerForm/PrayerForm.jsx
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import REACT, {useState} from 'react';
+import { useDispatch } from "react-redux";
+
+function prayerForm() {
+
+    const useDispatch = useDispatch();
+    const [user_id, setUser_Id] = useState("");
+    const [prayer_id, setPrayer_Id] = useState("");
+    const [mosque, setMosque] = useState("false");
+
+    
+   
+
+
+}
+
+export default prayerForm;
