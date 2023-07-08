33fca98b4111a2627337b85ec1c39b7d0ebd19ff
Mahamud Aden <mahamudadn@gmail.com>
2023-07-08 01:59:37 -0500

    added image and description in the about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutPage.css';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -8,9 +9,16 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
-      <div>
-        <p>This app is designed to help users track and monitor their prayers specifically performed at mosques. 
-          It allows users to keep a record of the number of prayers they have offered in mosques over time. This app enables users to evaluate their prayer performance and stay consistent with their religious practices.</p>
+      <div className="content-wrapper">
+        <img
+          src="./images/prayer.jpg"
+          alt="Description of the image"
+          className="custom-image"
+        />
+        <div className="text-wrapper">
+          <p>This app is designed to help users track and monitor their prayers specifically performed at mosques. 
+            It allows users to keep a record of the number of prayers they have offered in mosques over time. This app enables users to evaluate their prayer performance and stay consistent with their religious practices.</p>
+        </div>
       </div>
     </div>
   );
