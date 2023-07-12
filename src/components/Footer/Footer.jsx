477456f1f7124e4ab55fe698331c976311ea27f2
Mahamud Aden <mahamudadn@gmail.com>
2023-07-12 16:26:08 -0500

    added app name to my footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,12 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Mahamud Aden, 2023</footer>;
+  return <footer>GO M🕌SQUE &copy; Mahamud Aden, 2023
+
+{/* <p>Visit my: <a href="https://www.linkedin.com/in/mahamudaden//"target='_blank' >Linkedin </a></p> 
+     <br />
+      <p>Visit my: <a href="https://github.com/mahamudadn" target='_blank' >Github </a></p> */}
+  </footer>;
 }
 
 export default Footer;
